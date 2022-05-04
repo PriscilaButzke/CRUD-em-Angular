5b86b7a999dbe4816e04f26ecf6165b754b6044b
PriscilaButzke <priscilabutzke@outlook.com>
2022-05-04 12:00:49 -0300

    versão 3

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -23,13 +23,32 @@ export class CursosComponent implements OnInit {
 
     // instaciando um obejoto curso, assim ao escrever as informações nas inputs, vai gravar no 
     //objeto e gravar separamente em nome, valor e area.
-    
+    this.id = -1;
     this.curso = new curso("", 0, "");
     this.vetorCursos = this.servico.listar();
     
   }
   cadastrar(){
     this.servico.cadastrar(this.curso);
+    this.curso = new curso("", 0, "");
   }
 
+  excluir(id:number){
+    this.servico.excluir(id);
+    this.id = -1;
+  }
+  editar(id:number){
+    this.id = id;
+
+    this.curso = new curso(
+      this.vetorCursos[id].nomeCurso,
+      this.vetorCursos[id].valor,
+      this.vetorCursos[id].areaCurso
+    );
+  }
+  atualizar(){
+    this.servico.alterar(this.id, this.curso);
+    this.curso = new curso("", 0, "");
+    this.id = -1;
+  }
 }
